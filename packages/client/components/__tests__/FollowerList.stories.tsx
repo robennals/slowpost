437b76e6a7e66088f07e0bf03f03ef2f1379bfe7
isaacennalsbray <91916539+isaacennalsbray@users.noreply.github.com>
2025-10-05 00:12:13 -0700

    Add interaction plays to client stories

--- packages/client/components/__tests__/FollowerList.stories.tsx
+++ packages/client/components/__tests__/FollowerList.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { expect } from '@storybook/jest';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
 import { FollowerList } from '../FollowerList';
 import { sampleHome } from '../../lib/data';
 
@@ -14,10 +16,57 @@ export default meta;
 
 type Story = StoryObj<typeof FollowerList>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await expect(canvas.getAllByRole('listitem')).toHaveLength(2);
+    await expect(canvas.getByRole('textbox')).toHaveValue('Grace Hopper <grace@slowpost.org>');
+
+    const closeFriendsOnly = canvas.getByRole('checkbox', { name: /close friends only/i });
+    await userEvent.click(closeFriendsOnly);
+
+    await waitFor(() => expect(canvas.getAllByRole('listitem')).toHaveLength(1));
+
+    const graceItem = canvas.getByText('Grace Hopper').closest('li');
+    if (!graceItem) {
+      throw new Error('Grace Hopper list item not found');
+    }
+    const graceScope = within(graceItem);
+    await userEvent.click(graceScope.getByRole('checkbox', { name: /close friend/i }));
+
+    await waitFor(() => expect(canvas.queryAllByRole('listitem')).toHaveLength(0));
+    await waitFor(() => expect(canvas.getByRole('textbox')).toHaveValue(''));
+
+    await userEvent.click(closeFriendsOnly);
+    await waitFor(() => expect(canvas.getAllByRole('listitem')).toHaveLength(2));
+
+    const elonItem = canvas.getByText('Elon Slow').closest('li');
+    if (!elonItem) {
+      throw new Error('Elon Slow list item not found');
+    }
+    const elonScope = within(elonItem);
+    await userEvent.click(elonScope.getByRole('checkbox', { name: /close friend/i }));
+
+    await waitFor(() =>
+      expect(canvas.getByRole('textbox')).toHaveValue('Elon Slow <elon@slowpost.org>')
+    );
+  }
+};
 
 export const CloseFriendsOnly: Story = {
   args: {
     followers: sampleHome.followers.map((follower) => ({ ...follower, isCloseFriend: true }))
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    const closeFriendsOnly = canvas.getByRole('checkbox', { name: /close friends only/i });
+    await userEvent.click(closeFriendsOnly);
+
+    await waitFor(() => expect(canvas.getAllByRole('listitem')).toHaveLength(2));
+    await expect(canvas.getByRole('textbox')).toHaveValue(
+      'Grace Hopper <grace@slowpost.org>, Elon Slow <elon@slowpost.org>'
+    );
   }
 };
--- packages/client/components/__tests__/StatusBar.stories.tsx
+++ packages/client/components/__tests__/StatusBar.stories.tsx
@@ -1,4 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { expect } from '@storybook/jest';
+import { within } from '@storybook/testing-library';
 import { StatusBar } from '../StatusBar';
 
 const meta: Meta<typeof StatusBar> = {
@@ -13,11 +15,21 @@ export default meta;
 
 type Story = StoryObj<typeof StatusBar>;
 
-export const LoggedOut: Story = {};
+export const LoggedOut: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await expect(canvas.getByRole('link', { name: /slowpost/i })).toHaveAttribute('href', '/');
+    await expect(canvas.getByRole('link', { name: /login/i })).toHaveAttribute('href', '/p/login');
+  }
+};
 
 export const LoggedIn: Story = {
   args: {
     isLoggedIn: true,
     username: 'ada'
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await expect(canvas.getByRole('link', { name: /profile/i })).toHaveAttribute('href', '/ada');
   }
 };
