16d7fe93a39b07369a694eccd333414cc912d865
robennals <rob@ennals.org>
2025-10-12 20:47:14 -0700

    Storybook shows interactions panel

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-essentials'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/react-vite',
     options: {},
